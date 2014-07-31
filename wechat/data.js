3c7756346c3010b58fd68145bc14de38a8cc5c31
zhonglei qiu <mora@liulishuo.com>
2014-07-31 23:18:03 +0800

    添加微信数据

---
+++ wechat/data.js
@@ -0,0 +1,105 @@
+var DATA = {
+  // http://localhost/sites/fcbst/1/wechat/file_uploader.php
+  file_uploader_url: 'http://fcbst.sinaapp.com/wechat/file_uploader.php',
+  data_uploader_url: 'http://fcbst.sinaapp.com/wechat/upload_share_data.php',
+  share_url: 'http://fcbst.sinaapp.com/wechat/share.php?id=',
+  default_share_key: 'wechat_pro',
+
+  wechat_share_arrow_image: 'http://wechat-professor.qiniudn.com/wechat_top_right_share_arrow.png',
+  popular_share: {
+    kan_ni_you_duo_se: {
+      label: '看你有多色',
+      share_data: {
+        title: '看你有多色？',
+        desc: '我闯过95关，击败100%的人！我是【孤独求色lv95】,你敢来挑战吗？',
+        second_desc: '找出所有色块中颜色不同的一块。分享朋友圈，找到身边的色魔',
+        img_url: 'http://images.1758.com/images/wxicon.png',
+        link: 'http://www.1758.com/hlmy/yanse2.htm'
+      }
+    },
+    feng_kuan_de_shou_zi: {
+      label: '疯狂的手指',
+      share_data: {
+        title: '疯狂的手指',
+        desc: '10秒我能点543下！你行？你试试',
+        second_desc: '在10秒内疯狂的点按钮，想赢？不容易。',
+        img_url: 'http://wei.2sky.cn/vapp/27/3.jpg',
+        link: 'http://wei.2sky.cn/game/27/'
+      }
+    },
+    shang_hai_hua_6_test: {
+      label: '上海话专业六级考试',
+      share_data: {
+        title: '上海话专业六级考试',
+        desc: '我在上海话专业六级考试中得了100分，进来看看你能及格吗？',
+        second_desc: '我在上海话专业六级考试中得了100分，进来看看你能及格吗？',
+        img_url: 'http://wx.mingxg.com/resource/attachment/images/1/2014/07/n2183gUuOl9t181ztjlG9u6O82GO39.jpg',
+        link: 'http://wx.mingxg.com/mobile.php?act=module&id=5&name=dialect&do=detail&weid=1'
+      }
+    },
+    bao_da_shen_ji_mao: {
+      label: '暴打神经猫',
+      share_data: {
+        title: '暴打神经猫',
+        desc: '我打暴100只神经猫，不服来挑战！',
+        second_desc: '朋友圈被神经猫刷屏？快来暴打神经猫！',
+        img_url: 'http://ddqianphp.aliapp.com/game/img/dbsjm.jpg',
+        link: 'http://ddqianphp.aliapp.com/game/dbsjm'
+      }
+    },
+//    xxx: {
+//      label: '',
+//      share_data: {
+//        title: '',
+//        desc: '',
+//        second_desc: '',
+//        img_url: '',
+//        link: ''
+//      }
+//    },
+    shen_jing_mao: {
+      label: '围住神经猫',
+      share_data: {
+        title: '围住神经猫-根本停不下来，玩过之后我整个人都精神了！',
+        desc: '您用1步抓住了神经猫，击败了精神病院100%的精神病患者！',
+        second_desc: '在9x9范围内的格子中，使用色块围住白色神经猫。',
+        // http://1251001823.cdn.myqcloud.com/1251001823/wechat/mao80.jpg
+        img_url: 'http://wechat-professor.qiniudn.com/shen_jing_mao.png',
+        link: 'http://1251001823.cdn.myqcloud.com/1251001823/wechat/sjm/launcher'
+      }
+
+    },
+    mi_shi_tao_sheng: {
+      label: '密室逃生',
+      share_data: {
+        title: '密室逃生-9G游戏',
+        desc: '闯了100关！手心全是汗！这游戏实在TM太惊险了！谁敢来挑战我！',
+        second_desc: '尼玛，活着还真不容易',
+        //http://game.9g.com/kjxs/icon.png
+        img_url: 'http://wechat-professor.qiniudn.com/mi_shi_tao_sheng.png',
+        link: 'http://game.9g.com/msts/wx.html'
+      }
+    },
+    wechat_pro: {
+      label: '分享此站点',
+      share_data: {
+        title: '朋友圈装逼利器',
+        desc: '有此朋友圈装逼利器，谁还敢在我的地盘炫耀！赶紧收藏啰！',
+        second_desc: '朋友圈装逼利器！谁还敢在我我地盘炫耀！',
+        //http://game.9g.com/kjxs/icon.png
+        img_url: 'http://placekitten.com/g/80/80',
+        link: 'http://fcbst.sinaapp.com/wechat/builder'
+      }
+    },
+    self_custom: {
+      label: '自定义',
+      share_data: {
+        title: '',
+        desc: '',
+        second_desc: '',
+        img_url: 'http://placekitten.com/g/80/80',
+        link: ''
+      }
+    }
+  }
+};
